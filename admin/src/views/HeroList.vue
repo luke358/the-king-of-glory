020799e599caa79b7f7ef97b332c7e8d6713bea5
zxcbb <qq1494135711@gmail.com>
2021-02-22 17:16:21 +0800

    change router

--- admin/src/views/HeroList.vue
+++ admin/src/views/HeroList.vue
@@ -1,47 +1,60 @@
 <template>
   <div>
     <h1>英雄列表</h1>
-    <el-table
-      :data="
-        tableData.slice((currentPage - 1) * PageSize, currentPage * PageSize)
-      "
-    >
-      <el-table-column prop="_id" label="ID" width="240"> </el-table-column>
-
-      <el-table-column prop="name" label="英雄名称"> </el-table-column>
-      <el-table-column prop="title" label="称号"> </el-table-column>
-      <el-table-column prop="avatar" label="头像">
-        <template slot-scope="scope">
-          <img :src="scope.row.avatar" alt="" style="height:3rem" />
-        </template>
-      </el-table-column>
-
-      <el-table-column fixed="right" label="操作" width="180">
-        <template slot-scope="scope">
-          <el-button
-            type="text"
-            size="small"
-            @click="$router.push(`/heroes/edit/${scope.row._id}`)"
-            >编辑</el-button
-          >
-          <el-button type="text" size="small" @click="remove(scope.row)"
-            >删除</el-button
-          >
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="tabListPage">
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page="currentPage"
-        :page-sizes="pageSizes"
-        :page-size="PageSize"
-        layout="total, sizes, prev, pager, next, jumper"
-        :total="totalCount"
+    <el-card>
+      <el-row>
+        <el-col :span="6">
+          <el-input  maxlength="8" clearable placeholder="请输入英雄名称" v-model="heroQuery"></el-input>
+        </el-col>
+        <el-col :span="3">
+          <el-button style="margin-left:20px" type="primary" icon="el-icon-search" @click="searchHero">搜索</el-button>
+        </el-col>
+        <el-col :span="4" >
+          <el-button type="primary" icon="el-icon-plus" @click="addHeroClick">添加英雄</el-button>
+        </el-col>
+      </el-row>
+      <el-table
+        :data="
+          tableData.slice((currentPage - 1) * PageSize, currentPage * PageSize)
+        "
       >
-      </el-pagination>
-    </div>
+        <el-table-column prop="_id" label="ID" width="240"> </el-table-column>
+
+        <el-table-column prop="name" label="英雄名称"> </el-table-column>
+        <el-table-column prop="title" label="称号"> </el-table-column>
+        <el-table-column prop="avatar" label="头像">
+          <template slot-scope="scope">
+            <img :src="scope.row.avatar" alt="" style="height:3rem" />
+          </template>
+        </el-table-column>
+
+        <el-table-column fixed="right" label="操作" width="180">
+          <template slot-scope="scope">
+            <el-button
+              type="text"
+              size="small"
+              @click="$router.push(`/heroes/edit/${scope.row._id}`)"
+              >编辑</el-button
+            >
+            <el-button type="text" size="small" @click="remove(scope.row)"
+              >删除</el-button
+            >
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="tabListPage">
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="pageSizes"
+          :page-size="PageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="totalCount"
+        >
+        </el-pagination>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -88,7 +101,11 @@ export default {
     handleCurrentChange(val) {
       // 改变默认的页数
       this.currentPage = val;
-    }
+    },
+     // 添加英雄
+    addHeroClick() {
+      this.$router.push('/heroes/create')
+    },
   },
 
   created() {
--- admin/src/views/Main.vue
+++ admin/src/views/Main.vue
@@ -1,84 +1,105 @@
 <template>
   <el-container style="height: 100vh; ">
-    <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu router unique-opened :default-active="$route.path">
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-coin"></i>内容管理</template>
-
-          <el-menu-item-group>
-            <template slot="title">物品</template>
-            <el-menu-item index="/items/list">物品列表</el-menu-item>
-          </el-menu-item-group>
-
-          <el-menu-item-group>
-            <template slot="title">英雄</template>
-            <el-menu-item index="/heroes/create">新建英雄</el-menu-item>
-            <el-menu-item index="/heroes/list">英雄列表</el-menu-item>
-          </el-menu-item-group>
-
-          <el-menu-item-group>
-            <template slot="title">文章</template>
-            <el-menu-item index="/articles/create">新建文章</el-menu-item>
-            <el-menu-item index="/articles/list">文章列表</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title"
-            ><i class="el-icon-position"></i>运营管理</template
-          >
-
-          <el-menu-item-group>
-            <template slot="title">广告位</template>
-            <el-menu-item index="/ads/list">广告位列表</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-
-        <el-submenu index="3">
-          <template slot="title"
-            ><i class="el-icon-setting"></i>系统设置</template
-          >
-          <el-menu-item-group>
-            <template slot="title">分类</template>
-            <el-menu-item index="/categories/list">分类列表</el-menu-item>
-          </el-menu-item-group>
-
-          <el-menu-item-group>
-            <template slot="title">管理员</template>
-            <el-menu-item index="/admin_users/list">管理员列表</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-      </el-menu>
-    </el-aside>
+      <!-- 头部 -->
+    <el-header style="text-align: right; font-size: 12px">
+      <h2>
+        <span>王者荣耀后台管理系统</span>
+      </h2>
+      <el-button type="primary" size="small" @click="loginOut">退出</el-button>
+    </el-header>
 
     <el-container>
-      <el-header style="text-align: right; font-size: 12px">
-        <el-dropdown>
-          <i class="el-icon-setting" style="margin-right: 15px"></i>
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item @click.native="logout">退出</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-        <span>admin</span>
-      </el-header>
 
+      <!-- 侧边栏菜单 -->
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu router :default-active="activePath" unique-opened>
+          <!-- 内容管理一级菜单 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-s-fold"></i>分类管理
+            </template>
+            <!-- 内容管理二级菜单 -->
+            <el-menu-item-group>
+              <template slot="title">分类</template>
+              <el-menu-item index="/categories/list">分类列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 物品管理一级菜单 -->
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>物品管理
+            </template>
+            <!-- 物品管理二级菜单 -->
+            <el-menu-item-group>
+              <template slot="title">物品</template>
+              <el-menu-item index="/items/list">物品列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 英雄管理一级菜单 -->
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-s-help"></i>英雄管理
+            </template>
+            <!--英雄管理二级菜单 -->
+            <el-menu-item-group>
+              <template slot="title">英雄</template>
+              <el-menu-item
+                :index="$route.path"
+                v-if="$route.path==='/heroes/create'||$route.path.indexOf('/heroes/edit')!==-1"
+              >{{AddOrEdit}}</el-menu-item>
+              <el-menu-item index="/heroes/list">英雄列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 文章管理 -->
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-document"></i>文章管理
+            </template>
+            <!--文章管理二级菜单 -->
+            <el-menu-item-group>
+              <template slot="title">文章</template>
+              <el-menu-item
+                :index="$route.path"
+                v-if="$route.path==='/articles/create'||$route.path.indexOf('/articles/edit')!==-1"
+              >{{AddOrEdit}}</el-menu-item>
+              <el-menu-item index="/articles/list">文章列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 广告位管理 -->
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-s-opportunity"></i>广告位管理
+            </template>
+            <!--文章管理二级菜单 -->
+            <el-menu-item-group>
+              <template slot="title">广告位</template>
+              <el-menu-item index="/ads/list">广告位列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+
+          <!-- 用户管理 -->
+          <el-submenu index="6">
+            <template slot="title">
+              <i class="el-icon-user"></i>用户管理
+            </template>
+            <!--用户管理二级菜单 -->
+            <el-menu-item-group>
+              <template slot="title">用户</template>
+              <el-menu-item index="/admin_user/list">用户列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>
         <router-view :key="$route.path"></router-view>
       </el-main>
     </el-container>
   </el-container>
 </template>
-
-<style>
-.el-header {
-  background-color: #b3c0d1;
-  color: #333;
-  line-height: 60px;
-}
-
-.el-aside {
-  color: #333;
-}
-</style>
 
 <script>
 export default {
@@ -89,7 +110,7 @@ export default {
       address: "上海市普陀区金沙江路 1518 弄"
     };
     return {
-      tableData: Array(20).fill(item)
+      tableData: Array(20).fill(item),
     };
   },
   methods: {
@@ -97,6 +118,46 @@ export default {
       localStorage.removeItem("token");
       this.$router.push("/login");
     }
-  }
+  },
+  computed: {
+    activePath() {
+      return this.$route.path
+    },
+    AddOrEdit() {
+      if (this.$route.path.indexOf('/heroes') !== -1) {
+        if (this.$route.path === '/heroes/create') {
+          return '添加英雄'
+        } else if (this.$route.path.indexOf('/heroes/edit') !== -1) {
+          return '编辑英雄'
+        }
+      } else {
+        if (this.$route.path === '/article/create') {
+          return '添加文章'
+        } else if (this.$route.path.indexOf('/article/edit') !== -1) {
+          return '编辑文章'
+        }
+      }
+
+    }
+  },
 };
 </script>
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: linear-gradient(#11131f, #252a40);
+  color: #eee;
+}
+</style>
